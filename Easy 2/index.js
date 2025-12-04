24cdb6477457d2400572048466ecc267572bc998
Vaibhav Rajput <contact.vaibhavrajput.official@gmail.com>
2025-12-04 16:58:37 +0530

    changes

--- Easy 2/index.js
+++ Easy 2/index.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const app = express();
-
-
-app.get("/protected", (req, res) => {
-  // TODO: Send the response: Access granted at <timestamp>
-  
+function addTimestamp(req, res, next) {
+  req.timestamp = new Date();
+  next();
+}
+app.get("/protected", addTimestamp, (req, res) => {
+  res.send(`Access granted at ${req.timestamp}`);
 });
-
-
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
